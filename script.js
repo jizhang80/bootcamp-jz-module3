ec8095716291ee41b2ce896abb62e2c10cdf3319
jizhang80 <zhangji80@outlook.com>
2023-08-27 18:49:22 -0400

    clear content before generate new password

--- script.js
+++ script.js
@@ -119,10 +119,14 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.value = "";
+  setTimeout(function() {
+    var password = generatePassword();
+    passwordText.value = password;
+  },0);
+  
 
 }
 
